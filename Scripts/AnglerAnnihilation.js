47f80892b45bc64a438eed52ba21c9313466a378
amnicandri <amnicandri@imacnicandri-3.lan>
2020-12-10 10:18:33 -0800

    There is an error in the code that I am working on

--- Scripts/AnglerAnnihilation.js
+++ Scripts/AnglerAnnihilation.js
@@ -1,7 +1,74 @@
 anglerAnnihilator = {
-    init: function() {
-        //I made an edit - Isabelle//
-        let x = 5
+    fish: [],
+    init: function () {
+
+
+    },
+    createGreyFish: function () {
+        let grey = {
+            canSwim: Math.random(),
+        }
+        return grey
+    },
+    createYellowFish: function () {
+        let yellow = {
+            canSwim: Math.random(),
+        }
+        return yellow
+    },
+    createOrangeFish: function () {
+        let orange = {
+            canSwim: Math.random(),
+        }
+        return orange
+    },
+    createBlueFish: function () {
+        let blue = {
+            canSwim: Math.random(),
+        }
+        return blue
+    },
+    createGreenFish: function () {
+        let green = {
+            canSwim: Math.random(),
+        }
+        return green
+    },
+    createRedFish: function () {
+        let red = {
+            canSwim: Math.random(),
+        }
+        return red
+    },
+    createPurpleFish: function () {
+        let purple = {
+            canSwim: Math.random(),
+        }
+        return purple
+    },
+    createMagicFish: function () {
+        let Magic = {
+            canSwim: Math.random(),
+        }
+        return Magic
+    },
+    createBrownFish: function () {
+        let brown = {
+            canSwim: Math.random(),
+        }
+        return brown
+    },
+
+    startGame: function () {
+
+    },
+    endGame: function () {
+
+    },
+    swimFish: function () {
+        for (let i = 0; i < this.fish.length; i++) {
+            if (this.fish[i].canSwim )
+}
     }
 }
 
