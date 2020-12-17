96c8de1b52a2ce46d9dfcf11c0f4dcc171236d58
JoahG1 <jh73052@srcs.org>
2020-12-17 10:17:33 -0800

    Changes to everything

--- Scripts/AnglerAnnihilation.js
+++ Scripts/AnglerAnnihilation.js
@@ -1,69 +1,30 @@
 let anglerAnnihilator = {
     container: document.getElementById("animation_container"),
-    fish: [],
+    fishes: [],
+    fishTypeImages: [],
+
     init: function () {
-<<<<<<< HEAD
-        
-        //fishes.src = fish[randomNum()];
-=======
->>>>>>> e02a3606358eb19b8a6b23d0b6ff79c6cc3dc31f
 
-        this.renderFish();
-        this.startGame();
+        for (let i = 0; i < 10; i++) {
+            this.fishes.push(this.createFish());
+          }
 
-<<<<<<< HEAD
+      this.renderFish();
+      this.startGame();
     },
     createFish: function () {
-        let fishes = document.createElement("div")
-        fishes.className = "fish" 
-        let fishArray = [
-            {name: 'greyFish', image: "https://images.twinkl.co.uk/tr/image/upload/illustation/Swimming-Fish---Animated-Tail---Y2-Gymnastics-Under-the-Sea-Lesson-1---Balancing-Lesson-Pack-English---KS1.png"},
-            {name: 'yellowFish', image: "https://grid.gograph.com/tropical-cartoon-fish-eps-illustration_gg61604880.jpg"},
-            {name: 'orangeFish', image:"https://thumbs.dreamstime.com/b/pygoplites-diacanthus-aquarium-tropical-fish-144745023.jpg"},
-            {name: 'blueFish', image:"https://webstockreview.net/images/clipart-fish-teal-3.png"},
-            {name: 'greenFish', image:"https://thumbs.dreamstime.com/b/zanclus-cornutus-aquarium-tropical-fish-144745146.jpg"},
-            {name: 'redFish', image:"https://www.carlswebgraphics.com/fish-images/2020-red-fish-clipart.jpg"},
-            {name: 'purpleFish', image:"https://img.favpng.com/2/6/13/image-fish-portable-network-graphics-download-png-favpng-ebhYVWTEHGsTG2iafehsBLJP8.jpg"},
-            {name: 'magicFish', image:"https://webstockreview.net/images/clipart-borders-fish-8.png"},
-            {name: 'brownFish', image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRSggT2-afkjHeOQ0lVVWCCtlhtA8m-KQebg&usqp=CAU"}
-        ]
-
-        for (let i = 0; i < this.fish.length; i += 1) {
-            var fishImage = this.fish[i]
-            var pez = getRandomItem(fishArray)
-            this.fishArray[i].push(this.fish)
-            fish[i].innerHTML = pez.name + '<img src="' + pez.image + ' > '
-            document.getElementById('animation_container').appendChild(fishImage);
-        }
-=======
-
-       // let fishes = document.createElement("div")
-       // fishes.className = "fish"
-       // let fishArray = [
-        //    {name: 'greyFish', image: "https://images.twinkl.co.uk/tr/image/upload/illustation/Swimming-Fish---Animated-Tail---Y2-Gymnastics-Under-the-Sea-Lesson-1---Balancing-Lesson-Pack-English---KS1.png"},
-         //   {name: 'yellowFish', image: "https://grid.gograph.com/tropical-cartoon-fish-eps-illustration_gg61604880.jpg"},
-          //  {name: 'orangeFish', image:"https://thumbs.dreamstime.com/b/pygoplites-diacanthus-aquarium-tropical-fish-144745023.jpg"},
-          //  {name: 'blueFish', image:"https://webstockreview.net/images/clipart-fish-teal-3.png"},
-           // {name: 'greenFish', image:"https://thumbs.dreamstime.com/b/zanclus-cornutus-aquarium-tropical-fish-144745146.jpg"},
-          //  {name: 'redFish', image:"https://www.carlswebgraphics.com/fish-images/2020-red-fish-clipart.jpg"},
-           // {name: 'purpleFish', image:"https://img.favpng.com/2/6/13/image-fish-portable-network-graphics-download-png-favpng-ebhYVWTEHGsTG2iafehsBLJP8.jpg"},
-           // {name: 'magicFish', image:"https://webstockreview.net/images/clipart-borders-fish-8.png"},
-           // {name: 'brownFish', image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRSggT2-afkjHeOQ0lVVWCCtlhtA8m-KQebg&usqp=CAU"}
-      //  ]
-
-       // for (let i = 0; i < this.fish.length; i += 1) {
-        //    var pez = getRandomItem(fishArray)
-        //    this.fishArray[i].push(this.fish)
-        //    fish[i].innerHTML = pez.name + '<img src="' + pez.image + '>'
-        //}
-        //fishes.src = fish[randomNum()];
-
-        
-
-    },
-    createFish: function () {
-
->>>>>>> e02a3606358eb19b8a6b23d0b6ff79c6cc3dc31f
+    let fishdiv = document.createElement("div")
+    fishdiv.className = "fish"
+    this.container.append(fishdiv)
+    let fish = {
+        x_pos: Math.random() * 580,
+        y_pos: Math.random() * 580,
+        x_velocity: Math.random() * 1,
+        y_velocity: Math.random() * .5,
+        radius: 5,
+        element: fishdiv,
+    }
+    return fish;
     },
 
     startGame: function () {
@@ -74,27 +35,20 @@ let anglerAnnihilator = {
     },
 
     swimFish: function () {
-        for (let i = 0; i < this.fish.length; i++) {
-            if (this.fish[i].canSwim == true) {
+        for (let i = 0; i < this.fishes.length; i++) {
+            if (this.fishes[i].canSwim == true) {
 
             }
         }
     },
     renderFish: function () {
-        for (let i = 0; i < this.fish.length; i++) {
-            this.fish[i].element.style.top = this.fish[i].y_pos + "px"
-            this.fish[i].element.style.left = this.fish[i].x_pos + "px"
+        for (let i = 0; i < this.fishes.length; i++) {
+            console.log("renderingFish")
+            this.fishes[i].element.style.top = this.fishes[i].y_pos + "px"
+            this.fishes[i].element.style.left = this.fishes[i].x_pos + "px"
         }
     },
-    startAnimation: function () {
-        var frames = document.getElementById("animation").children;
-        var frameCount = frames.length;
-        var i = 0;
-        setInterval(function () { 
-            frames[i % frameCount].style.display = "none";
-            frames[++i % frameCount].style.display = "block";
-        }, 100);
-    }
+    
 
     //randomNum: function () {
     //    random = Math.random() * 8;
@@ -104,5 +58,4 @@ let anglerAnnihilator = {
 
 
 }
-
-MyApp.init();
+anglerAnnihilator.init();
